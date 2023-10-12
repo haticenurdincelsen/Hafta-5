824994e69ba8fbff07d5d05b5b6edee814a098b0
haticenurdincelsen <142350794+haticenurdincelsen@users.noreply.github.com>
2023-10-12 12:17:36 +0300

    Kitap-Siralayici

---
+++ KitapSiralayici/src/OrderNameComperator.java
@@ -0,0 +1,6 @@
+public class OrderNameComperator implements Comparable<Book>{
+    @Override
+    public int compareTo(Book o) {
+        return 0;
+    }
+}
---
+++ KitapSiralayici/src/order.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class order implements Comparator <Book> {
+    @Override
+    public int compare(Book o1, Book o2) {
+        return 0;
+    }
+}
